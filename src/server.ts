2fd9eeb216399a245b84c576461ec5e90d651877
prince <princevip720@gmail.com>
2023-10-07 17:49:39 +0600

    husky setup

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 async function booStrap() {
   try {
-    await mongoose.connect(config.mongoURI as string);
-    console.log("Connected to database");
+    await mongoose.connect(config.mongoURI as string)
+    console.log('Connected to database')
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`);
-    });
+      console.log(`Application listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log("Error connecting to database: ", error);
+    console.log('Error connecting to database: ', error)
   }
 }
 
-booStrap();
+booStrap()
